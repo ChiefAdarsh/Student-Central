b1d27e860d47f596e1e317ba300bf26fab877179
Adarsh Goura <adarshgoura5@gmail.com>
2022-11-28 13:37:25 -0600

    New Files

---
+++ Student Central/Contact Counselor.swift
@@ -0,0 +1,29 @@
+//
+//  Contact Counselor.swift
+//  Student Central
+//
+//  Created by Adarsh Goura on 11/28/22.
+//
+
+import UIKit
+
+class Contact_Counselor: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Student Central/Student Help.swift
@@ -0,0 +1,29 @@
+//
+//  Student Help.swift
+//  Student Central
+//
+//  Created by Adarsh Goura on 11/28/22.
+//
+
+import UIKit
+
+class Student_Help: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
