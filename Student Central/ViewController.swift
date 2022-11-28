182800b78fe6969af399664cef0084f9e291fae8
Adarsh Goura <adarshgoura5@gmail.com>
2022-11-28 12:24:24 -0600

    Initial Commit

---
+++ Student Central/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Central
+//
+//  Created by Adarsh Goura on 11/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
