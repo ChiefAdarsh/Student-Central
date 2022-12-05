da2cf1176c59a4b0ee9cff54298f45742eeee6f9
Omesh Sana <omesh.reddy18@gmail.com>
2022-12-05 13:43:03 -0600

    Mon Dec 5 1:42 PM

--- Student Central/Student Help.swift
+++ Student Central/Student Help.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class HelpViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let size = UIScreen.main.bounds.size
+        if size.height < size.width {
+            stackView.axis = .horizontal
+        } else {
+            stackView.axis = .vertical
+        }
         // Do any additional setup after loading the view.
         
     }
-    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        guard let _ = stackView else { return }
+        
+        if UIDevice.current.orientation.isLandscape {
+            stackView.axis = .horizontal
+        } else {
+            stackView.axis = .vertical
+        }
+    }
 
     /*
     // MARK: - Navigation
