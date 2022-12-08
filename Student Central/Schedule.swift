92980c115d28a260513a94ad8e7245bd36c03ec8
Pranav <pranav.harwadekar@hotmail.com>
2022-12-08 15:39:23 -0600

    Class Schedule and Shuttle Schedule Revised - 12/08/22

--- Student Central/Schedule.swift
+++ Student Central/Schedule.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ScheduleViewController: UIViewController {
+class ClassScheduleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,4 +26,41 @@ class ScheduleViewController: UIViewController {
     }
     */
 
+}
+
+class ShuttleViewController: UIViewController {
+    @IBOutlet var route1: UILabel!
+    @IBOutlet var sv: UIStackView!
+    @IBOutlet var route2: UILabel!
+    
+    func rotation() {
+        let size = UIScreen.main.bounds.size
+        if size.height < size.width {
+            sv.axis = .horizontal
+            sv.spacing = 97
+        } else {
+            sv.axis = .vertical
+            sv.spacing = 50
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        rotation()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rotation()
+        
+        let textRange1 = NSMakeRange(0, route1.text!.count)
+        let attributedText1 = NSMutableAttributedString(string: route1.text!)
+        attributedText1.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: textRange1)
+        route1.attributedText = attributedText1
+        
+        let textRange2 = NSMakeRange(0, route2.text!.count)
+        let attributedText2 = NSMutableAttributedString(string: route2.text!)
+        attributedText2.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: textRange2)
+        route2.attributedText = attributedText2
+    }
 }
---
+++ Student Central/ShuttleSchedule.swift
@@ -0,0 +1,10 @@
+//
+//  ShuttleSchedule.swift
+//  Student Central
+//
+//  Created by Pranav Harwadekar on 12/8/22.
+//
+
+import Foundation
+
+class
