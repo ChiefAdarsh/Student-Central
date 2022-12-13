57669fce10da07209bd396ed87e98de64ca882dc
Pranav <pranav.harwadekar@hotmail.com>
2022-12-13 14:11:00 -0600

    Working Class Schedule 12/13

--- Student Central/Schedule.swift
+++ Student Central/Schedule.swift
@@ -8,24 +8,12 @@
 import UIKit
 
 class ClassScheduleViewController: UIViewController {
+    @IBOutlet var segue: UISegmentedControl!
+    @IBOutlet var sv: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 class ShuttleViewController: UIViewController {
