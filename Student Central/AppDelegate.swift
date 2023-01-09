f8585367845cbced52b8a853cc2c588ecf709d98
Mukund Raman <mkr9416@g.coppellisd.com>
2023-01-09 14:29:54 -0600

    FIREBASE #2

--- Student Central/AppDelegate.swift
+++ Student Central/AppDelegate.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GIDSignIn.sharedInstance.clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance.delegate = self
         return true
     }
 
